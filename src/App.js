2d0d16a349b46b5400df55f1b90bbd8fe754b790
소빈 <sobin11@naver.com>
2024-11-27 12:43:41 +0900

    🚧 temporarily comment out section5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Section01 from "./components/Section01";
 import Section02 from "./components/Section02";
 import Section03 from "./components/Section03";
 import Section04 from "./components/Section04";
-import Section05 from "./components/Section05";
+// import Section05 from "./components/Section05";
 import Footer from "./components/Footer";
 //
 import "./style.css";
@@ -34,7 +34,7 @@ function App() {
         <Section02 />
         <Section03 />
         <Section04 />
-        <Section05 />
+        {/* <Section05 /> */}
       </main>
       <Footer />
     </div>
